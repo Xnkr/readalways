3c9ac866bf25ab08bfd948917e9e2b3dab733230
Xnkr <26054028+Xnkr@users.noreply.github.com>
2017-08-24 12:32:46 +0530

    Finished UI of Home,Inbox,Accounts

--- start.bat
+++ start.bat
@@ -1,2 +1,9 @@
 subl
 python manage.py runserver
+@ECHO OFF
+CHOICE /C YN /M "git push?"
+IF ERRORLEVEL ==2 exit
+IF ERRORLEVEL ==1 goto push
+
+:PUSH
+echo "hello"
---
+++ static/js/script.js
@@ -0,0 +1,5 @@
+$(document).ready(function() {
+    $('.approve').click(function() {
+        return confirm("Are you sure you want to send your Email address to the requester?");
+    })
+});
