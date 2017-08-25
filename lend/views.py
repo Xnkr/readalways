ce6ddbf46cced0aa409e9dd214e234c0c384a725
Xnkr <26054028+Xnkr@users.noreply.github.com>
2017-08-26 00:56:06 +0530

    Added UI for Book add and edit. Added modal for Book add. Fixed security bugs for edit and delete

--- lend/views.py
+++ lend/views.py
@@ -9,6 +9,8 @@ from django import forms
 from django.urls import reverse
 from . import models
 from borrow import models as b_models
+
+from django.contrib import messages
 # Create your views here.
 
 IMAGE_FILE_TYPES = ['jpg','png','jpeg']
@@ -54,23 +56,31 @@ def create_book(request):
 
 @login_required
 def delete_book(request, book_id):
-    book = models.Book.objects.get(pk=book_id)
-    book.delete()
-    books = models.Book.objects.filter(book_lender=request.user.pk)
-    context = {
-				'user': request.user,
-				'books' : models.Book.objects.filter(book_lender=request.user.pk),
-			}
-    return render(request, 'lend/index.html', context)
+	book = models.Book.objects.get(pk=book_id)
+	if book.book_lender == request.user:
+		book.delete()
+		books = models.Book.objects.filter(book_lender=request.user.pk)
+		context = {
+		'user': request.user,
+		'books' : models.Book.objects.filter(book_lender=request.user.pk),
+		}
+	else:
+		messages.error(request, 'You cannot delete what you did not add.')
+		return redirect('lend:lend_home')
+	return render(request, 'lend/index.html', context)
 
 @login_required
 def update_book(request,book_id):
 	instance = get_object_or_404(models.Book, id=book_id)
-	form = BookForm(request.POST or None, request.FILES or None, instance=instance)
-	if form.is_valid():
-		form.save()
-		return redirect('lend:lend_home')
-	return render(request, 'lend/create_book.html', {'form': form})
+	if instance.book_lender == request.user:
+		form = BookForm(request.POST or None, request.FILES or None, instance=instance)
+		if form.is_valid():
+			form.save()
+			return redirect('lend:lend_home')
+		return render(request, 'lend/create_book.html', {'form': form})
+	else:
+		messages.error(request, 'You cannot edit what you did not add.')
+    	return redirect('lend:lend_home')
 
 @login_required
 def details(request,book_id):
@@ -90,24 +100,24 @@ def details(request,book_id):
 def approve(request,req_id):
 	book_requested = get_object_or_404(b_models.BookRequest, id=req_id)
 	if not request.user == book_requested.request_receiver:
-		return HttpResponse("You do not have permissions to authorize",status=403)
+		messages.error(request, 'That request was not yours to approve')
 	else:
 		book_requested.approved = True
 		book_requested.save()
 		book = book_requested.book_request
 		book.book_borrowable = False
 		book.save()
-		return redirect('inbox')
+	return redirect('inbox')
 
 @login_required
 def reject(request,req_id):
 	book_requested = get_object_or_404(b_models.BookRequest, id=req_id)
 	if not request.user == book_requested.request_receiver:
-		return HttpResponse("You do not have permissions to authorize",status=403)
+		messages.error(request, 'That request was not yours to reject')
 	else:
 		book_requested.approved = False
 		book_requested.save()
 		book = book_requested.book_request
 		book.book_borrowable = True
 		book.save()
-		return redirect('inbox')
+	return redirect('inbox')
